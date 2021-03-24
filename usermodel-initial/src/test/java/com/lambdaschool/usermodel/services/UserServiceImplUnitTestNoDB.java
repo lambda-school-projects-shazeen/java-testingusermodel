228e10614176d64a51735e576878fd68e05c333d
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-24 14:20:51 -0700

    set up find all test

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplUnitTestNoDB.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplUnitTestNoDB.java
@@ -1,17 +1,120 @@
 package com.lambdaschool.usermodel.services;
 
+import com.lambdaschool.usermodel.UserModelApplicationTesting;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.repository.UserRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplicationTesting.class,
+    properties = {"command.line.runner.enabled=false"})
 public class UserServiceImplUnitTestNoDB
 {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private HelperFunctions helperFunctions;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private List<User> userList;
 
     @Before
     public void setUp() throws Exception
     {
+        userList = new ArrayList<>();
+
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
+
+        r1 = roleService.save(r1);
+        r2 = roleService.save(r2);
+        r3 = roleService.save(r3);
+
+        // admin, data, user
+        User u1 = new User("admin",
+            "password",
+            "admin@lambdaschool.local");
+        u1.setUserid(10);
+        u1.getRoles()
+            .add(new UserRoles(u1,
+                r1));
+        u1.getRoles()
+            .add(new UserRoles(u1,
+                r2));
+        u1.getRoles()
+            .add(new UserRoles(u1,
+                r3));
+        u1.getUseremails()
+            .add(new Useremail(u1,
+                "admin@email.local"));
+        u1.getUseremails()
+            .add(new Useremail(u1,
+                "admin@mymail.local"));
+
+        userList.add(u1);
+
+        // data, user
+        User u2 = new User("cinnamon",
+            "1234567",
+            "cinnamon@lambdaschool.local");
+        u2.setUserid(20);
+
+        u2.getRoles()
+            .add(new UserRoles(u2,
+                r2));
+        u2.getRoles()
+            .add(new UserRoles(u2,
+                r3));
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "cinnamon@mymail.local"));
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "hops@mymail.local"));
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "bunny@email.local"));
+        userList.add(u2);
+
+        // user
+        User u3 = new User("barnbarn",
+            "ILuvM4th!",
+            "barnbarn@lambdaschool.local");
+        u3.setUserid(30);
+
+        u3.getRoles()
+            .add(new UserRoles(u3,
+                r2));
+        u3.getUseremails()
+            .add(new Useremail(u3,
+                "barnbarn@email.local"));
+        userList.add(u3);
+
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
@@ -32,6 +135,9 @@ public class UserServiceImplUnitTestNoDB
     @Test
     public void findAll()
     {
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
+        System.out.println(userService.findAll());
+        assertEquals(3, userService.findAll().size());
     }
 
     @Test
